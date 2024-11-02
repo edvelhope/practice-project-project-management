fb6db9020505caf11d42342db0b3f4260cb9a5ed
Edoardo <edoardoderosaa@gmail.com>
2024-11-02 23:21:47 +0100

    Clear tasks and fix minor bugs

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -11,11 +11,11 @@ export default function Tasks({ tasks, onAdd, onDelete }) {
         </p>
       )}
       {tasks.length > 0 && (
-        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+        <ul className="p-2 mt-8">
           {tasks.map((task) => (
-            <li key={task.id} className="flex justify-between my-4">
+            <li key={task.id} className="flex justify-between mt-3 p-2 rounded-lg bg-stone-100">
               <span>{task.text}</span>
-              <button className="text-stone-700 hover:text-red-500">
+              <button className="text-stone-700 hover:text-red-500" onClick={() => onDelete(task.id)}>
                 Clear
               </button>
             </li>
